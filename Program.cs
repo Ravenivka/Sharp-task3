58e1dcb83134f79b8944ce237218c789b5ec47d3
Raven <raveniv@ya.ru>
2023-12-28 15:57:50 +0500

    output added

--- Program.cs
+++ Program.cs
@@ -8,11 +8,28 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        static int Maximka(int number)
+        {
+            int A = number / 10;
+            int B = number % 10;
+            if (A > B)
+            {
+                return A;
+            }else if(A==B)
+            {
+                return A;
+            }
+            else
+            {
+                return B;
+            }
+        }
         static void Main()
         {
             Console.WriteLine("Введите целое число из отрезка [10, 99]");
             int N = Int32.Parse(Console.ReadLine());
-           
+            string msg = "Наибольшая цифра: " + Maximka(N);
+            Console.WriteLine(msg);
         }
     }
 }
